4ed41893bb0f83b48d8cad12f633f3b4869484b6
Andrew Howard <andrew.bryce.howard@gmail.com>
2023-07-24 01:59:24 +0000

    implment random and some cleanup

--- mysql_variant/bender.py
+++ mysql_variant/bender.py
@@ -1,14 +1,21 @@
 import os
 import time
-from random import randint
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-from cobe.brain import Brain
+from cobe import brain
+# from cobe.brain import Brain
+# import MySQLdb
+# from MySQLdb.constants import CR as MySQLdb_CR
 from dotenv import load_dotenv
 
 load_dotenv()
 
 settings = {}
+# settings["MYSQL_HOST"]            = os.getenv("MYSQL_HOST")
+# settings["MYSQL_PORT"]            = int(os.getenv("MYSQL_PORT"))
+# settings["MYSQL_USERNAME"]        = os.getenv("MYSQL_USERNAME")
+# settings["MYSQL_ROOT_PASSWORD"]   = os.getenv("MYSQL_ROOT_PASSWORD")
+# settings["MYSQL_DB"]              = os.getenv("MYSQL_DB")
 settings["BOT_TOKEN"]             = os.getenv("SLACK_BOT_TOKEN")
 settings["BOT_ID"]                = os.getenv("SLACK_BOT_TOKEN").split(':')[0]
 settings["APP_TOKEN"]			  = os.getenv("SLACK_APP_TOKEN")
@@ -18,7 +25,7 @@ settings["TRIGGER_ENABLED"]       = bool(os.getenv("TRIGGER_ENABLED"))
 settings["TRIGGER_WORDS"]         = os.getenv("TRIGGER_WORDS").split(",")
 settings["CHAT_ALLOWLIST"]        = os.getenv("CHAT_ALLOWLIST").split(",")
 settings["RANDOM_ENABLED"]        = bool(os.getenv("RANDOM_ENABLED"))
-settings["RANDOM_RATIO"]          = os.getenv("RANDOM_RATIO")
+settings["RANDOM_PERCENTAGE"]     = os.getenv("RANDOM_PERCENTAGE")
 settings["PRIVATE_REPLY_ENABLED"] = bool(os.getenv("PRIVATE_REPLY_ENABLED"))
 settings["BLOCKLIST_WORDS"]       = os.getenv("BLOCKLIST_WORDS").split(",")
 settings["LEARN_ENABLED"]         = bool(os.getenv("LEARN_ENABLED"))
@@ -26,7 +33,25 @@ settings["LEARN_ENABLED"]         = bool(os.getenv("LEARN_ENABLED"))
 # Initializes your app with your bot token and socket mode handler
 app = App(token=settings["BOT_TOKEN"])
 
-bender = Brain('bender.brain')
+# brain = None
+# while not brain:
+# 	try:
+# 		brain = Brain(settings["mysql_host"], settings["mysql_port"], settings["mysql_username"], settings["mysql_root_password"], settings["mysql_db"])
+# 	except MySQLdb._exceptions.OperationalError as e:
+# 		if e.args[0] != MySQLdb_CR.CONN_HOST_ERROR:
+# 			raise
+# 		brain = None
+# 		print("Waiting for DB to go UP...")
+# 		time.sleep(20)
+
+# print("CONNECTED TO DB!")
+
+# try:
+# 	bender = brain.Brain('bender.brain')
+# except brain.CobeError as err:
+# 	print("brain dead", err)
+
+bender = brain.Brain('bender.brain')
 
 # UTILITY FUNCTIONS
 # Remove the first "trigger word" detected if string is beginning with it
@@ -43,17 +68,24 @@ def start_with_trigger_words(string):
 			return string[:len(word)]
 	return None
 
+# Listens to incoming messages that contain "hello"
 # To learn available listener arguments,
 # visit https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
 @app.message('.*')
 def message_learn(message, say):
 	msg = message['text']
-	if settings["LEARN_ENABLED"]:
-	    bender.learn(remove_trigger_words(msg))
-	if 'bender' in msg.lower() or (settings["RANDOM_ENABLED"] and randint(0, settings["RANDOM_RATIO"])==0):
+	bender.learn(msg)
+	if 'bender' in msg.lower():
 		reply = bender.reply(msg)
     	# say() sends a message to the channel where the event was triggered
 		say(reply)
+
+
+# @app.message("bender")
+# def message_reply(message, say):
+#     reply = bender.reply(message['text'])
+#     # say() sends a message to the channel where the event was triggered
+#     say(reply)
 
 # Start your app
 if __name__ == "__main__":
